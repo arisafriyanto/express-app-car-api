abc0f2125c2336e529081a2c11ea720714fc377e
arisafriyanto <arisafriyanto1933@gmail.com>
2023-12-23 16:59:29 +0700

    fix: yaml file and server

--- server.ts
+++ server.ts
@@ -29,7 +29,7 @@ class Server {
         return ResponseBuilder.response({
             res,
             code: 404,
-            message: 'resource, data or page not found, go go go',
+            message: 'resource, data or page not found',
             data: 'not found',
         });
     }
